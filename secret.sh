d9640b2610c3678905bd24f053fb146624b033cb
Jia Hao <lonoise62@gmail.com>
2023-07-05 19:33:50 +0800

    added remove newline

--- secret.sh
+++ secret.sh
@@ -5,6 +5,6 @@ for SECRET_NAME in "${ARR[@]}"
 do
     vlt secrets get --plaintext $SECRET_NAME > input.txt;
     output="$(base64 -w 0 < input.txt)";
-    echo $output;
-    # kubectl patch secret $SECRETS_NAME -p="{\"data\":{\"$SECRET_NAME\": \"$output\"}}" -n $namespace -v=1;
+    echo "Patching secret $SECRET_NAME";
+    kubectl patch secret $SECRETS_NAME -p="{\"data\":{\"$SECRET_NAME\": \"$output\"}}" -n $namespace -v=1;
 done
