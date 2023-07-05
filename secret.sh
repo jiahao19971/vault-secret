f65428b01937bde96d4c7c4758ea4527999103a9
Jia Hao <lonoise62@gmail.com>
2023-07-05 19:16:50 +0800

    echo output

--- secret.sh
+++ secret.sh
@@ -3,7 +3,7 @@ declare -a ARR=$ALL_SECRET
 
 for SECRET_NAME in "${ARR[@]}"
 do
-    output="$(vlt secrets get --plaintext $SECRET_NAME | base64)";
+    output="$(vlt secrets get --plaintext $SECRET_NAME | base64 -e)";
     echo $output;
     kubectl patch secret $SECRETS_NAME -p="{\"data\":{\"$SECRET_NAME\": \"$output\"}}" -n $namespace -v=1;
 done
