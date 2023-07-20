011c5764973f5d9e0c1c24d4bd4d3145a192f0c8
Jia Hao <lonoise62@gmail.com>
2023-07-20 09:52:32 +0800

    update vault

--- secret.py
+++ secret.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
       decoded = get_output.strip().decode()
       print(f"Patching secret {conf['key']}") 
       data = json.dumps({"data": {conf['key']: decoded}})
-      patch_output = subprocess.check_output(["kubectl", "patch", "secret", config['name'], f"-p={data}", "-n", config['namespace'], "-v=1"])
+      patch_output = subprocess.check_output(["kubectl", "patch", "secret", config['name'], f'-p="{data}"', "-n", config['namespace'], "-v=1"])
       print(patch_output)
   except Exception as e:
       print(e)
